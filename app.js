e37516cb13411549ba6f8f52d0f1d313f3672115
nitesh <nitesh.khatri88@gmail.com>
2023-04-02 21:18:14 +0530

    added feature typescript

---
+++ app.js
@@ -0,0 +1,105 @@
+// let uname = new String("Hello avaTpoint");  
+// console.log("Message: " + uname);  
+// console.log("Length: "+uname.length);
+// var studentName:string = 'sameet khatri is student name'
+// console.log(studentName)
+// const obj = { width: 10, height: 15 };
+// console.log(obj)
+// // types | number | string 
+// var a = 10
+// console.log(a)
+// let b:number = 10
+// console.log(b)
+// const area = obj.width * obj.height;
+// console.log(area)
+// interface User{
+//     name:string;
+//     id:number;
+// }
+// class UserAccount{
+//     name:string;
+//     id:number;
+//     constructor(name:string,id:number){
+//         this.name = name;
+//         this.id = id;
+//     }
+// }
+// let abc:number = 10.11
+// console.log(abc)
+// let data:string[] = ['nitesh','sameet','arvind',10,100]
+// data[3] = true
+// console.warn(data)
+// let users = {
+//     name:'nitesh',
+//     age:33,
+//     address:'amir house, grant road east, mumbai, india'
+// }
+// console.warn(users);
+// users.name = 'sameet'
+// users.age = 40
+// users.address = 'pingrove chicago IL, USA'
+// console.log(users)
+// interface usersTyped {
+//     name:string;
+//     age:number;
+//     address:string;
+// }
+// let users:usersTyped = {
+//     name:'nitesh',
+//     age:33,
+//     address:'amir huse'
+// }
+// console.log(users)
+// interface jobtitle{
+//     teamname:string,
+//     empid:number
+// }
+// let company:jobtitle[] = [
+//     {
+//         teamname:'nitesh',
+//         empid:1,
+//     },
+//     {
+//         teamname:'vishal',
+//         empid:2,
+//     },
+//     {
+//         teamname:'sameet',
+//         empid:3,
+//     },
+// ]
+// console.log(company)
+// let person:any=[
+//     {
+//         name:'vishal',
+//         country:"india"
+//     },
+//     {
+//         name:'sonal',
+//         country:"india"
+//     },
+//     {
+//         name:'nilesh',
+//         country:"india"
+//     },
+// ]
+// person.name='sameet'
+// person.country='usa'
+// console.log(person)
+// let data:string= 'nitesh khatri'
+// console.warn(data)
+// let data:string|number|boolean = 'nitesh khatri'
+// console.warn(data)
+// interface userType {
+//     name:string;
+//     age:number;
+//     getName:() => string
+// }
+// let users:userType = {
+//     name:'nitesh khatri',
+//     age:30,
+//     getName:function(){
+//         return 'sameet khatri'
+//     }
+// }
+// console.warn(users.getName())
